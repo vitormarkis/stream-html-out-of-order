ddf9a3be710979a26979cfc7f0bdb378541ac33e
vtrmrks <vtrmrks3@gmail.com>
2024-06-07 06:56:30 +0000

    out of order streaming done

--- src/server.ts
+++ src/server.ts
@@ -4,33 +4,73 @@ import { sleep } from "./sleep"
 
 const app = express()
 
-app.get("/:pokemon_id", (req, res) => {
+app.get("/:pokemon_id", async (req, res) => {
   const { pokemon_id } = req.params
   const stream = new Readable({
     read() {},
   })
 
+  const promises = new Set()
+
+  let streamController: ReadableStreamDefaultController<string>
+
+  const responseStream = new ReadableStream({
+    start(controller) {
+      streamController = controller
+    },
+  })
+
   stream.push(`
-    <h1>Pokemon name:</h1>
-    <strong id="fallback">Loading...</strong>
+    <h1>Pokemon:</h1>
+    <strong id="fallback_name">Loading name...</strong> <br>
+    <strong id="fallback_effect">Loading effect...</strong>
   `)
 
-  fetch(`https://pokeapi.co/api/v2/ability/${pokemon_id}`)
+  const namePromise = fetch(`https://pokeapi.co/api/v2/ability/${pokemon_id}`)
     .then(res => res.json())
     .then(async data => {
-      await sleep(3000)
-      stream.push(`
+      await sleep(1400)
+
+      streamController.enqueue(`
         <strong id="pokemon_name">${data.name}</strong>
         <script>
-          const fallback = document.getElementById("fallback")
+          const fallbackName = document.getElementById("fallback_name")
           const pokemonName = document.getElementById("pokemon_name")
-          fallback.replaceWith(pokemonName)
+          fallbackName.replaceWith(pokemonName)
         </script>
       `)
-      stream.push(null)
+      promises.delete(namePromise)
+      if (promises.size === 0) streamController.close()
     })
+  promises.add(namePromise)
+
+  const effectPromise = fetch(`https://pokeapi.co/api/v2/ability/${pokemon_id}`)
+    .then(res => res.json())
+    .then(async data => {
+      await sleep(3400)
+      const effect = data.effect_entries.find(
+        (entry: any) => entry.language.name === "en"
+      ).effect
+
+      streamController.enqueue(`
+        <strong id="pokemon_effect">${effect}</strong>
+        <script>
+          const fallbackEffect = document.getElementById("fallback_effect")
+          const pokemonEffect = document.getElementById("pokemon_effect")
+          fallbackEffect.replaceWith(pokemonEffect)
+        </script>
+      `)
+      promises.delete(effectPromise)
+      if (promises.size === 0) streamController.close()
+    })
+  promises.add(effectPromise)
 
   stream.pipe(res)
+  // @ts-expect-error
+  for await (const chunk of responseStream) {
+    stream.push(chunk)
+  }
+  stream.push(null)
 })
 
 app.listen(4000, () => {
