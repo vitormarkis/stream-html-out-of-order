da5ff51dafa8f2dc6626279fcb683113eacade74
vtrmrks <vtrmrks3@gmail.com>
2024-06-08 02:34:23 +0000

    response with html

--- src/server.ts
+++ src/server.ts
@@ -1,43 +1,17 @@
 import express from "express"
-import { Readable } from "node:stream"
-import { sleep } from "./sleep"
-import { sleep } from "./sleep"
 
 const app = express()
 
-app.get("/pokemon/:pokemon_id", (req, res) => {
+app.get("/pokemon/:pokemon_id", async (req, res) => {
   const { pokemon_id } = req.params
-  const stream = new Readable({
-    read() {},
-  })
-
-  stream.push(`
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/ability/${pokemon_id}`
+  )
+  const data = await response.json()
+  return res.send(`
     <h1>Pokemon name:</h1>
-    <strong id="fallback">Loading...</strong>
+    <strong id="pokemon_name">${data.name}</strong>
   `)
-
-  fetch(`https://pokeapi.co/api/v2/ability/${pokemon_id}`)
-    .then(res => res.json())
-    .then(async data => {
-      await sleep(3000)
-      stream.push(`
-        <strong id="pokemon_name">${data.name}</strong>
-        <script>
-          const fallback = document.getElementById("fallback")
-          const pokemonName = document.getElementById("pokemon_name")
-          fallback.replaceWith(pokemonName)
-        </script>
-      `)
-      stream.push(null)
-    })
-  promises.add(effectPromise)
-
-  stream.pipe(res)
-  // @ts-expect-error
-  for await (const chunk of responseStream) {
-    stream.push(chunk)
-  }
-  stream.push(null)
 })
 
 app.listen(4000, () => {
