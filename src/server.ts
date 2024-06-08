dc61b6f7868b16bf145c22bbb29fcd33f85082e4
vtrmrks <vtrmrks3@gmail.com>
2024-06-08 02:34:52 +0000

    stream suspense

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,36 @@
 import express from "express"
+import { Readable } from "node:stream"
+import { sleep } from "./sleep"
 
 const app = express()
 
-app.get("/pokemon/:pokemon_id", async (req, res) => {
+app.get("/pokemon/:pokemon_id", (req, res) => {
   const { pokemon_id } = req.params
-  const response = await fetch(
-    `https://pokeapi.co/api/v2/ability/${pokemon_id}`
-  )
-  const data = await response.json()
-  return res.send(`
+  const stream = new Readable({
+    read() {},
+  })
+
+  stream.push(`
     <h1>Pokemon name:</h1>
-    <strong id="pokemon_name">${data.name}</strong>
+    <strong id="fallback">Loading...</strong>
   `)
+
+  fetch(`https://pokeapi.co/api/v2/ability/${pokemon_id}`)
+    .then(res => res.json())
+    .then(async data => {
+      await sleep(3000)
+      stream.push(`
+        <strong id="pokemon_name">${data.name}</strong>
+        <script>
+          const fallback = document.getElementById("fallback")
+          const pokemonName = document.getElementById("pokemon_name")
+          fallback.replaceWith(pokemonName)
+        </script>
+      `)
+      stream.push(null)
+    })
+
+  stream.pipe(res)
 })
 
 app.listen(4000, () => {
